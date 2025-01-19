80836b40589d41b7993eb8b9532668550df95862
awabalish <awabalishah@gmail.com>
2025-01-20 04:56:25 +0500

    Fix deployment issues: Update routing and build configuration

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
+import React from 'react';
 import "@fontsource/andada-pro"; 
 import './App.css';
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
 import HomePage from "./components/homepage/homePage";
 import SignIn from "./components/auth/SignIn";
 import SignUp from "./components/auth/SignUp";
@@ -10,16 +12,16 @@ import { ThemeProvider } from "@material-tailwind/react";
 function App() {
   return (
     <ThemeProvider>
-      <div className="App">
-        <BrowserRouter>
+      <Router>
+        <div className="App">
           <Routes>
-            <Route path="/" exact element={<HomePage/>} />
-            <Route path="/signin" element={<SignIn/>} />
-            <Route path="/signup" element={<SignUp/>} />
-            <Route path="/forgot-password" element={<ForgotPassword/>} />
+            <Route path="/" element={<HomePage />} />
+            <Route path="/signin" element={<SignIn />} />
+            <Route path="/signup" element={<SignUp />} />
+            <Route path="/forgot-password" element={<ForgotPassword />} />
           </Routes>
-        </BrowserRouter>
-      </div>
+        </div>
+      </Router>
     </ThemeProvider>
   );
 }
